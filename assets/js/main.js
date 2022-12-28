9ea14725b3027a4e9e9011226414a20b7f728d44
ThuanVan Nguyen <60531224+Nguyen-Van-Thuan@users.noreply.github.com>
2022-12-28 20:58:30 +0700

    update feature show and hidden popup

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function handleOpen() {
+    // alert('1');
+    document.querySelector(".wrap-chat").style.bottom = '80px';
+}
+function handleClose() {
+    // alert('2');
+    document.querySelector(".wrap-chat").style.bottom = '-520px';
+}
